05287d5a2361a463e83446efdfcfe2241522bae1
blendInIt <135830356+jayword69@users.noreply.github.com>
2024-08-12 02:52:24 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Example of a simple script to handle form submissions
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! We will get back to you shortly.');
+});
